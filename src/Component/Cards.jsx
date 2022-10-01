7068b1d695a34aefb07cbd6a9e77060cf71f810e
harsh007-cmyk <83878921+harsh007-cmyk@users.noreply.github.com>
2022-10-01 22:52:11 +0530

    Add files via upload

---
+++ src/Component/Cards.jsx
@@ -0,0 +1,46 @@
+import React,{useState,useEffect} from 'react'
+import './cards.css'
+import Star from './Star';
+const CardData=[];
+function Cards(props) {
+  console.log('props',props);
+ let [cardItem,setcardItem]=useState('');
+function handleCart(){
+  console.log(props.cartItem)
+  let cartNum=Number(props.cartItem);
+  props.setFunction(cartNum+1);
+}
+  useEffect(() => {
+    fetch("https://acciojob-module-7-sept-2022-default-rtdb.asia-southeast1.firebasedatabase.app/products.json")
+    .then(response => response.json())
+    .then(data => setcardItem(data))
+  },[])
+  for(const item in cardItem){
+    CardData.push(cardItem[item])
+  } 
+  const CardBoxes=CardData.map((item)=>{
+    return (
+      <div className='cardItem'>
+        <img src={item.productImage} alt="" />
+        <div className='productName'>{item.productName}</div>
+        <div className='starAndPrice'>
+          <div className='star'>
+            <Star/>
+          </div>
+        <div className='Price'>
+        <div className='oldPrice'>{item.oldPrice}</div>
+        <div className='newPrice'>{item.newPrice}</div>
+        </div>
+        </div>
+
+        <button onClick={handleCart} className='addToCart'>ADD TO CART</button>
+      </div>
+    )
+  })
+  
+  return (
+    <div className='cards'>{CardBoxes}</div>
+  )
+}
+
+export default Cards
